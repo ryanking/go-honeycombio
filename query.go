558db7b8d9b207e79ef08e448f4c4525d1859236
Ryan King <ryan.king@color.com>
2022-03-14 16:16:32 -0700

    add query result method

--- query.go
+++ query.go
@@ -3,6 +3,7 @@ package honeycombio
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 // Queries describe all the query-related methods that the Honeycomb API
@@ -16,6 +17,8 @@ type Queries interface {
 	// Create a new query in this dataset. When creating a new query ID may
 	// not be set.
 	Create(ctx context.Context, dataset string, c *QuerySpec) (*QuerySpec, error)
+
+	GetQueryResult(ctx context.Context, dataset string, queryID string) (*QueryResult, error)
 }
 
 // queries implements Queries.
@@ -36,4 +39,36 @@ func (s *queries) Create(ctx context.Context, dataset string, data *QuerySpec) (
 	var q QuerySpec
 	err := s.client.performRequest(ctx, "POST", "/1/queries/"+urlEncodeDataset(dataset), data, &q)
 	return &q, err
+}
+
+func (s *queries) GetQueryResult(ctx context.Context, dataset string, queryID string) (*QueryResult, error) {
+	q := struct {
+		QueryID string `json:"query_id"`
+	}{
+		QueryID: queryID,
+	}
+	r := struct {
+		ID       string `json:"id"`
+		Complete bool   `json:"complete"`
+	}{}
+
+	err := s.client.performRequest(ctx, "POST", fmt.Sprintf("/1/query_results/%s", urlEncodeDataset(dataset)), q, &r)
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.client.performRequest(ctx, "GET", fmt.Sprintf("/1/query_results/%s/%s", urlEncodeDataset(dataset), r.ID), nil, &r)
+
+	return nil, err
+}
+
+type QueryResult struct {
+	ID       string `json:"id"`
+	Complete bool   `json:"complete"`
+	Data     []struct {
+		Series []struct {
+			Time time.Time `json:"time"`
+			Data map[string]interface{}
+		}
+	}
 }
